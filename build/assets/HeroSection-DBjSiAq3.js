1c025e65aebff97db7b15a271ffef9347630db33
Surya-Teja-Varma <kalidindisuryatejavarma@gmail.com>
2026-02-13 16:23:38 +0530

    feat: Add new website build output including a HeroSection with a particle background.

--- build/assets/HeroSection-DBjSiAq3.js
+++ build/assets/HeroSection-DBjSiAq3.js
@@ -1,4 +1,4 @@
-import{c as A,j as t,t as z,a as C,r as w}from"./index-D_eVD41f.js";/**
+import{c as A,j as t,t as z,a as C,r as w}from"./index-Bt110abV.js";/**
  * @license lucide-react v0.487.0 - ISC
  *
  * This source code is licensed under the ISC license.
